6a2bd59200020ad82fd15ebbf8a60fc00e2d65aa
Sangam Jangid <sangamjangid8@gmail.com>
2025-07-06 19:17:41 +0530

    Fix: Collect static files for deploy

--- RoyalStore/settings.py
+++ RoyalStore/settings.py
@@ -124,8 +124,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Static files (CSS, JavaScript, Images)
