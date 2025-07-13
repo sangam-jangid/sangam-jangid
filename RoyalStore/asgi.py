8075bb4929e9e07a0e0f32b16329369d74395ab1
Sangam Jangid <sangamjangid8@gmail.com>
2025-07-13 11:17:06 +0530

    fix WhiteNoise media path error

--- RoyalStore/asgi.py
+++ RoyalStore/asgi.py
@@ -10,17 +10,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RoyalStore.settings')
 
 django_asgi_app = get_asgi_application()
 
+from django.conf import settings
 if not settings.DEBUG:
     from whitenoise import WhiteNoise
 
     django_asgi_app = WhiteNoise(django_asgi_app, root=settings.STATIC_ROOT)
-    django_asgi_app.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
+    if os.path.isdir(settings.MEDIA_ROOT):
+        django_asgi_app.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
